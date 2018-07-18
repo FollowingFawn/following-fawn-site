d1cecbd888b91c6c846e7823bb5d388a83f8212b
Fawn Stevens <fstevens@usclermg5568.global.publicisgroupe.net>
2018-07-18 14:50:31 -0700

    Committing general webpage layout

--- following-fawn-site/src/App.js
+++ following-fawn-site/src/App.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import Header from './components/headerComponent/header';
+import Footer from './components/footerComponent/footer';
+import Homepage from './components/pages/homePage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header/>
+        <Footer/>
       </div>
     );
   }
